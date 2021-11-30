831acb736bd2a93d067ea2e701636c9ac64914ff
yxnne <506524339@qq.com>
2021-12-01 01:00:36 +0800

    test: 修改测试用例

--- src/hooks/use-mount/__Test__/index.test.tsx
+++ src/hooks/use-mount/__Test__/index.test.tsx
@@ -1,99 +1,102 @@
 import { renderHook } from "@testing-library/react-hooks";
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 import { useMount } from "../index";
 
 type WrapperPropsType = {
-    step: number;
-    children?: ReactNode
-}
+  step: number;
+  children?: ReactNode;
+};
 
-describe('useMount', () => {
+describe("useMount", () => {
+  it("简单组件挂载时执行一次", () => {
+    let count = 0;
 
-    it('简单组件挂载时执行一次', () => {
-        let count = 0;
+    renderHook(() =>
+      useMount(() => {
+        count++;
+      })
+    );
 
-        const { rerender, result } = renderHook(
-            () => useMount(() => {
-                count++;
-            })
-        )
+    expect(count).toEqual(1);
+  });
 
-        expect(count).toEqual(1);
+  it("组件Props改变不会重复执行", () => {
+    let count = 0;
+
+    const { rerender } = renderHook(
+      () =>
+        useMount(() => {
+          count++;
+        }),
+      {
+        initialProps: {
+          name: "csg",
+        },
+      }
+    );
+
+    rerender({
+      name: "csg strong",
     });
 
-    it('组件Props改变不会重复执行', () => {
-        let count = 0;
+    expect(count).toEqual(1);
+  });
 
-        const { rerender, result } = renderHook(
-            () => useMount(() => {
-                count++;
-            }),
-            {
-                initialProps: {
-                    name: 'csg'
-                }
-            }
-        )
+  it("unmount之后组件rerender会重复执行", () => {
+    let count = 0;
 
-        rerender({
-            name: 'csg strong'
-        })
+    const { rerender, unmount } = renderHook(() =>
+      useMount(() => {
+        count++;
+      })
+    );
+    unmount();
+    rerender();
+    expect(count).toEqual(2);
+  });
 
-        expect(count).toEqual(1);
-    })
+  it("父组件Props改变不会重复执行", () => {
+    let count = 0;
+    const DIV: React.FC<WrapperPropsType> = ({ children, step }) => {
+      return (
+        <>
+          <div>{children}</div>
+          <div>{step}</div>
+        </>
+      );
+    };
 
-    it('unmount之后组件rerender会重复执行', () => {
-        let count = 0;
+    const { rerender } = renderHook(
+      () =>
+        useMount(() => {
+          count++;
+        }),
+      {
+        wrapper: DIV,
+        initialProps: {
+          step: 1,
+        },
+      }
+    );
+    rerender({ step: 2 });
+    expect(count).toEqual(1);
+  });
 
-        const { rerender, unmount } = renderHook(
-            () => useMount(() => {
-                count++;
-            })
-        )
-        unmount()
-        rerender()
-        expect(count).toEqual(2);
-    })
+  it("先挂载再卸载，继续挂载，执行2次", () => {
+    let count = 0;
+    const { unmount, rerender } = renderHook(() =>
+      useMount(() => {
+        count++;
+      })
+    );
+    unmount();
+    // renderHook(() =>
+    //   useMount(() => {
+    //     count++;
+    //   })
+    // ).unmount();
 
-    it('父组件Props改变不会重复执行', () => {
-        let count = 0;
-        const DIV: React.FC<WrapperPropsType> = ({ children, step }) => {
-            return (
-                <>
-                    <div>{children}</div>
-                    <div>{step}</div>
-                </>
-            )
-        }
-
-        const { rerender, unmount } = renderHook(
-            () => useMount(() => {
-                count++;
-            }), {
-            wrapper: DIV,
-            initialProps: {
-                step: 1
-            }
-        }
-        )
-        rerender({ step: 2 })
-        expect(count).toEqual(1);
-    })
-
-    it('先挂载再卸载，继续挂载，执行2次', () => {
-        let count = 0;
-        const { rerender, unmount } = renderHook(
-            () => useMount(() => {
-                count++;
-            })
-        )
-        unmount()
-        renderHook(
-            () => useMount(() => {
-                count++;
-            })
-        ).unmount()
-        expect(count).toEqual(2);
-    })
-
-})
+    rerender()
+    expect(count).toEqual(2);
+  });
+});
--- src/hooks/use-unmount/__Test__/index.test.tsx
+++ src/hooks/use-unmount/__Test__/index.test.tsx
@@ -1,30 +1,35 @@
 import { renderHook } from "@testing-library/react-hooks";
-import React from 'react';
+import React from "react";
 import { useUnmount } from "../index";
 
-describe('useUnmount', () => {
+describe("useUnmount", () => {
+  it("应该非undefined", () => {
+    expect(useUnmount).toBeDefined();
+  });
 
-    it('应该非undefined', () => {
-        expect(useUnmount).toBeDefined()
-    })
+  it("test unmount", () => {
+    /**
+     * 1. 卸载的时候执行一次
+     * 2. rerender 时不会重新执行
+     * 2. 卸载又挂载再卸载执行两次
+     */
 
-    it('test unmount', () => {
+    // jest.fn(): jest 最简单的mock函数，如果不定义参数则返回undefined
+    const fn = jest.fn();
+    const { unmount, rerender } = renderHook(() => useUnmount(fn));
+    unmount();
+    expect(fn).toBeCalledTimes(1);
 
-        /**
-         * 1. 卸载的时候执行一次
-         * 2. rerender 时不会重新执行
-         * 2. 卸载又挂载再卸载执行两次
-         */
+    rerender();
+    expect(fn).toBeCalledTimes(1);
 
-        // jest.fn(): jest 最简单的mock函数，如果不定义参数则返回undefined
-        const fn = jest.fn();
-        const hook = renderHook(() => useUnmount(fn))
-        hook.unmount()
-        expect(fn).toBeCalledTimes(1)
-        hook.rerender()
-        expect(fn).toBeCalledTimes(1)
-        renderHook(() => useUnmount(fn)).unmount()
-        expect(fn).toBeCalledTimes(2)
-    })
+    unmount();
+    expect(fn).toBeCalledTimes(2);
 
-})
+    rerender();
+    expect(fn).toBeCalledTimes(2);
+
+    unmount();
+    expect(fn).toBeCalledTimes(3);
+  });
+});

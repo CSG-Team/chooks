a4ed0ada5c6bf1b0790a7b5c8a2286b189ef4519
林琪琪 <865530219@qq.com>
2021-12-03 01:16:00 +0800

    style: 删除多余的代码

--- src/hooks/use-set-state/__Test__/index.test.tsx
+++ src/hooks/use-set-state/__Test__/index.test.tsx
@@ -7,43 +7,6 @@ describe("useSetState", () => {
     expect(useSetState).toBeDefined();
   });
 
-/*
-  // 注释的 看完之后可以删掉
-
-  it("test useSetState", () => {
-    const { result, unmount, rerender } = renderHook(() =>
-      useSetState<State>({
-        hello: "",
-        count: 0,
-      })
-    );
-    const [state, setState] = result.current;
-    setState({ hello: "world" });
-    setTimeout(() => {
-
-
-      // 随便写个东西这个测试用例也能过
-      // 异步的 
-      // jest里面要用done函数
-      // 否则全部通过
-      // 不信可以放开注释试试
-      expect(state.hello).toBe("lin777 随便写个东西这个测试用例也能过～");
-
-    }, 0);
-
-    setState((preState) => preState.count + 1);
-    setTimeout(() => {
-      expect(state.count).toBe(1);
-    }, 0);
-
-    setState({ name: "csg", hello: "hello" });
-    setTimeout(() => {
-      expect(state.name).toBe("csg");
-      expect(state.name).toBe("hello");
-    }, 0);
-  });
-  */
-
 
   // 初始化hooks
   function initRender<T extends object>(initState: T) {
@@ -52,13 +15,13 @@ describe("useSetState", () => {
       return {
         state,
         setState,
-      } as const; 
+      } as const;
     })
   }
 
 
   it("支持初始化值", () => {
-    const initState = { name: 'csg', slogan:'f**k!' }
+    const initState = { name: 'csg', slogan: 'f**k!' }
 
     const { result } = initRender(initState)
 
@@ -70,7 +33,7 @@ describe("useSetState", () => {
 
 
   it('支持更新对象', () => {
-    const { result } = initRender<{name?:string; core?:string}>({
+    const { result } = initRender<{ name?: string; core?: string }>({
       name: 'csg',
     });
     // 要用act
@@ -83,7 +46,7 @@ describe("useSetState", () => {
 
 
   it('支持函数式更新', () => {
-    const { result } = initRender<{name?:string; core?:string}>({
+    const { result } = initRender<{ name?: string; core?: string }>({
       name: 'csg',
     });
 
